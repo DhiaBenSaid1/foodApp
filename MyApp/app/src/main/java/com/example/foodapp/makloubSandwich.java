3f967c2f2f97d49a494c826ba6f7c082a4bbee49
dhia ben said <bensaiddhia432@gmail.com>
2024-04-23 20:09:14 +0100

    third commit

---
+++ MyApp/app/src/main/java/com/example/foodapp/makloubSandwich.java
@@ -0,0 +1,14 @@
+package com.example.foodapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class makloubSandwich extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_makloub_sandwich);
+    }
+}
